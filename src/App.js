398b3c8dca4c30708678860169b7881b94c3792f
MC-Guenette <>
2021-04-15 11:47:12 -0500

    Added Ticket QA page with links + separated components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import React, {useState} from 'react';
 import 'react-pro-sidebar/dist/css/styles.css';
 import Dashboard from '../src/pages/dashboard';
 import AppFrame from '@bold-commerce/bevy-react/lib/components/appframe/AppFrame';
 import Sidebar from '@bold-commerce/bevy-react/lib/components/sidebar/Sidebar';
 import Nav from '@bold-commerce/bevy-react/lib/components/nav/Nav';
+import TicketQa from './pages/ticketqa';
 //import Page from  '@bold-commerce/bevy-react/lib/components/page/Page';
-function App() {
 
-  function setPage(string) {
-    alert(string)
-  
+function App() {
+  const [currentPage, setCurrentPage] = useState('DASHBOARD');
+
+  function setPage(page) {
+    setCurrentPage(page);
   }
 
   return (
@@ -17,13 +20,18 @@ function App() {
       <AppFrame>
 <Sidebar title="Internal Hub" collapsable>  
       <Nav aria-label="three link nav">
-  <Nav.Item active iconBefore="home" label="Dashboard" href="#" onClick={ e => e.preventDefault() } aria-label="first-nav-item" />
-  <Nav.Item iconBefore="edit" label="Ticket QA" href="#" onClick={ e => e.preventDefault() } />
+  <Nav.Item active iconBefore="home" label="Dashboard" href="#" onClick={ e => setPage('DASHBOARD') } aria-label="first-nav-item" />
+  <Nav.Item iconBefore="edit" label="Ticket QA" onClick={ e => setPage('TICKET_QA') } />
   <Nav.Item iconBefore="person" label="My Account" href="#" onClick={ e => e.preventDefault() } />
 </Nav>
 </Sidebar>
-<Dashboard/>
- 
+{/* <Dashboard/> */}
+{
+  {
+   'DASHBOARD': <Dashboard/>, 
+   'TICKET_QA': <TicketQa/>
+  } [currentPage]
+}
 </AppFrame>
 
     </div>
--- src/pages/ticketqa.js
+++ src/pages/ticketqa.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import Card from '@bold-commerce/bevy-react/lib/components/card/Card';
-import InputField from '@bold-commerce/bevy-react/lib/components/inputfield/InputField';
-import Grid from '@bold-commerce/bevy-react/lib/components/grid/Grid';
-import Button from '@bold-commerce/bevy-react/lib/components/button/Button';
-<Page.AnnotatedSegment
-title="Account information"
-details="This information is all related, but needs a bit of separation because there's a bit of difference between the email address and the passwords sections."
->
-<Card sectioned>
-  <Card.Section>
-    <h3>Log-in details</h3>
-    <InputField label="Email address" />
-  </Card.Section>
-  <Card.Section>
-    <h3>Reset password</h3>
-    <Grid>
-      <Grid.Half>
-        <InputField label="Password" type="password" />
-      </Grid.Half>
-      <Grid.Half>
-        <InputField label="Confirm password" type="password" />
-      </Grid.Half>
-    </Grid>
-  </Card.Section>
-</Card>
-</Page.AnnotatedSegment>
+import Page from  '@bold-commerce/bevy-react/lib/components/page/Page';
+import QaForm from './components/ticketqa/qaform';
+
+function TicketQa() {
+  return(
+      <Page>
+      <Page.Header
+        title="Ticket QA"
+        description="Quality Assurance - Merchant Success"
+        icon="edit"
+        breadcrumbs={[{
+          href: "javascript:;",
+          text: "Home"
+        },{
+          href: "javascript:;",
+          text: "Ticket QA"
+        }]}
+          />
+<QaForm/>
+      </Page>
+  )
+}
+
+export default TicketQa;
